919aceae4b8cdc35e6b9293b973c1306cb037a2d
NUR HASANAH <80372457+ananhs12@users.noreply.github.com>
2021-10-06 22:25:39 +0800

    Add files via upload

---
+++ RecyclerView-master/app/src/main/java/com/example/recyclerview/MyItem.java
@@ -0,0 +1,23 @@
+package com.example.recyclerview;
+
+public class MyItem {
+
+    static int[] iconList = {
+            R.drawable.facebook, R.drawable.instagram, R.drawable.twitter, R.drawable.tiktok, R.drawable.firefox,
+            R.drawable.whatsapp, R.drawable.telegram, R.drawable.youtube, R.drawable.messenger, R.drawable.opera
+    };
+
+    static String[] Headline= {
+            "Facebook", "Instagram", "Twitter", "TikTok", "Mozilla Firefox",
+            "WhatsApp", "Telegram", "YouTube", "Messenger", "Opera"
+            //menambahkan aplikasi sosial media
+    };
+
+    static String[] Subheadline= {
+            "Social Media", "Social Media", "Social Media", "Social Media", "Browser",
+            "Chat App", "Chat App", "Video Streaming", "Chat Apps", "Browser"
+            //membuat kalimat peralihan yang mengarahkan pembaca dari judul
+    };
+
+
+}
